ac4240c8a2446d8690aa40a2ec2ebcbea24cf7f1
jean luc kawel <jeanluckawel45@yahoo.com>
2022-08-21 00:30:18 +0200

    while boucle

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 $lignes=1;
 while ($lignes<=100){
-    echo "suis un developeur web<br>";
+    echo "<h3>suis un developeur web</h3>";
     $lignes++;
 }
+?>
+<style>
+    h1,h3{
+        font-family: "open sans";
+        text-align: center;
+        padding: 0;
+        margin: 0;
+    }
+</style>
+<?php
+echo "<h1>hello</h1>";
