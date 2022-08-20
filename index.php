44942919877a41bef9bca22d8dc63657039ec463
jean luc kawel <jeanluckawel45@yahoo.com>
2022-08-21 00:15:20 +0200

    tableau

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
-$garde=10;
-switch ($garde){
-    case 2:
-        echo "lol codeur";
-        break;
-    case 3:
-        echo "tlc";
-        break;
-    default:
-        echo "pas de chance";
-
-}
+$user=['jlk','097445354','rdc'];
+echo "$user[0]";
